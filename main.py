da1219bfa60802e42835a3148d4192e6ced0c2aa
Rafael <rafael.angeles.toledano@gmail.com>
2025-04-22 05:58:11 -0300

    chore:cleaning files, removing useless lines

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from pdf_processor.repository import PostgreSQLRepository
 
 def process_all_pdfs(folder_path: str):
     if not os.path.exists(folder_path):
-        print(f"❌ Pasta não encontrada: {folder_path}")
+        print(f"Pasta não encontrada: {folder_path}")
         return
 
     reader = PDFReader()
@@ -18,15 +18,15 @@ def process_all_pdfs(folder_path: str):
             try:
                 text = reader.read(file_path)
                 data = extractor.extract(text)
-                
+
                 if not data["full_name"] or not data["birth_date"]:
-                    raise ValueError("❗ Nome ou data de nascimento vazios. Verifique o conteúdo do PDF.")
+                    raise ValueError("Nome ou data de nascimento vazios. Verifique o conteúdo do PDF.")
 
                 repository.save(data)
-                print("✅ Sucesso ao salvar:", data["full_name"])
+                print("Sucesso ao salvar:", data["full_name"])
 
             except Exception as e:
-                print(f"❌ Erro ao processar {file}: {e}")
+                print(f"Erro ao processar {file}: {e}")
 
 if __name__ == "__main__":
     pasta_pdfs = os.path.join("data", "prontuarios")
